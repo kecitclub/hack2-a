ec20daae4dfab5664cdb6c56d24181c1b2000fda
Satish <dhakalsatish2004@gmail.com>
2025-01-09 12:47:39 +0545

    general

--- basobash/components/Navbar.jsx
+++ basobash/components/Navbar.jsx
@@ -1,6 +1,25 @@
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
+      <div className="text-lg font-bold">Rental Finder</div>
+      <ul className="flex space-x-4">
+        <li>
+          <a href="#" className="hover:underline">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#features" className="hover:underline">
+            Features
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className="hover:underline">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
