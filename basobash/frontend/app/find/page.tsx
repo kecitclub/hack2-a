33114657a477776c41bf684dde3eaec361f989bd
Samridh Kandel <samridh.8848@gmail.com>
2025-01-10 11:06:04 +0545

    finalized listing

--- basobash/frontend/app/find/page.tsx
+++ basobash/frontend/app/find/page.tsx
@@ -1,15 +1,15 @@
-import GeocodedFindMap from "@/components/geocodedmap";
 import { title } from "@/components/primitives";
 import AutoCompleteSearch from "@/utils/AutoCompleteSearch";
 import "leaflet/dist/leaflet.css";
 import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import GeocodedMap from "@/components/geocodedmap";
 
 export default function page() {
   return (
     <div className="w-[80vw] h-[50vh]">
       <h1 className={`text-3xl font-semibold`}>Find</h1>
       <div className="p-5">
-        <GeocodedFindMap />
+        <GeocodedMap />
       </div>
     </div>
   );
