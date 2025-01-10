d461915b091a8ec1ed1ec1c54dcf53bcafaac6fc
Supreme Khadka <supremekhadka2@gmail.com>
2025-01-10 17:53:14 +0545

    properties route to app dir

--- basobash/frontend/pages/api/auth/signup.ts
+++ basobash/frontend/pages/api/auth/signup.ts
@@ -1,29 +1,37 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import bcrypt from 'bcryptjs';
-import dbConnect from '../../../lib/mongodb';
-import User from '../../../models/User';
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcryptjs";
+import dbConnect from "../../../lib/mongodb";
+import User from "../../../models/User";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') return res.status(405).end();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") return res.status(405).end();
 
-    const { phone, password, description, sex } = req.body;
+  const { phone, password, description, sex } = req.body;
 
-    try {
-        await dbConnect();
+  try {
+    await dbConnect();
 
-        const existingUser = await User.findOne({ phone });
-        console.log(existingUser);
-        
-        if (existingUser) return res.status(400).json({ error: 'User already exists' });
+    const existingUser = await User.findOne({ phone });
+    console.log(existingUser);
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+    if (existingUser)
+      return res.status(400).json({ error: "User already exists" });
 
-        const user = await User.create({phone: phone, password: hashedPassword, description: description, sex: sex})
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        res.status(201).json({ message: 'User created successfully', user: user });
-    } catch (error) {
-        console.error('Error during signup:', error); // Log the error
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-    
+    const user = await User.create({
+      phone: phone,
+      password: hashedPassword,
+      description: description,
+      sex: sex,
+    });
+
+    res.status(201).json({ message: "User created successfully", user: user });
+  } catch (error) {
+    console.error("Error during signup:", error); // Log the error
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
